c3830fb2dc76f99df51c00bedfaf9870c714b3cb
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2018-10-02 17:51:16 +0200

    Updates

---
+++ precache-manifest.f2b3b95c90a4aa0f61201a3b9da8e7ed.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "12f27ec13dba0cbd3889",
+    "url": "/inline-clippath-converter/static/js/runtime~main.12f27ec1.js"
+  },
+  {
+    "revision": "375aacae1286578f0c9e",
+    "url": "/inline-clippath-converter/static/js/main.375aacae.chunk.js"
+  },
+  {
+    "revision": "08fd9658c4c45d917631",
+    "url": "/inline-clippath-converter/static/js/1.08fd9658.chunk.js"
+  },
+  {
+    "revision": "e72a678e4330b0fd2336c25fe4d1757e",
+    "url": "/inline-clippath-converter/index.html"
+  }
+];
